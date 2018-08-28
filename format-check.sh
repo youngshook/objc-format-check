1997379d69fc0c531acdafa2c35c0354c99e52cf
Young Shook <shook.young@gmail.com>
2018-08-28 16:02:17 +0800

    Update repo URL

--- format-check.sh
+++ format-check.sh
@@ -8,7 +8,7 @@ then rm -rf .spacecommander
 fi
 mkdir .spacecommander
 cd .spacecommander
-git clone https://github.com/Bupterambition/objc-format-check.git spacecommander
+git clone https://github.com/youngshook/objc-format-check.git spacecommander
 cd $orgin
 bash "$(pwd)/.spacecommander/spacecommander/setup-repo.sh"
 exit 0
